64ae3bafcdfa38a48b068f94798d4236459a9f0c
Nathan L <nlandmanc@gmail.com>
2022-08-20 11:36:28 +0100

    ALMOST THERE

--- chain-code/contracts/WavePortal.sol
+++ chain-code/contracts/WavePortal.sol
@@ -6,17 +6,85 @@ import "hardhat/console.sol";
 
 contract WavePortal {
     uint256 totalWaves;
+    uint256 private seed;
 
-    constructor() {
-        console.log("Yo yo, I am a contract and I am smart");
+    /*
+     * A little magic, Google what events are in Solidity!
+     */
+    event NewWave(address indexed from, uint256 timestamp, string message);
+
+    /*
+     * I created a struct here named Wave.
+     * A struct is basically a custom datatype where we can customize what we want to hold inside it.
+     */
+    struct Wave {
+        address waver; // The address of the user who waved.
+        string message; // The message the user sent.
+        uint256 timestamp; // The timestamp when the user waved.
     }
 
-    function wave() public {
+    /*
+     * I declare a variable waves that lets me store an array of structs.
+     * This is what lets me hold all the waves anyone ever sends to me!
+     */
+    Wave[] waves;
+
+    constructor() payable {
+        console.log("I AM SMART CONTRACT. POG.");
+        seed = (block.difficulty + block.timestamp + seed) % 100;
+    }
+
+    /*
+     * This is an address => uint mapping, 
+     * meaning I can associate an address with a number!
+     * In this case, I'll be storing the address with the 
+     * last time the user waved at us.
+     */
+    mapping(address => uint256) public lastWavedAt;
+
+    function wave(string memory _message) public {
+        require(
+            lastWavedAt[msg.sender] + 30 seconds < block.timestamp,
+            "Wait 30s at least!"
+        );
+
+        lastWavedAt[msg.sender] = block.timestamp;
+
         totalWaves += 1;
         console.log("%s has waved!", msg.sender);
+
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+        console.log("Random # generated: %d", seed);
+
+        if (seed < 50) {
+            console.log("%s won!", msg.sender);
+
+            /*
+             * The same code we had before to send the prize.
+             */
+            uint256 prizeAmount = 0.0001 ether;
+            require(
+                prizeAmount <= address(this).balance,
+                "Trying to withdraw more money than the contract has."
+            );
+            (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+            require(success, "Failed to withdraw money from contract.");
+        }
+
+        emit NewWave(msg.sender, block.timestamp, _message);
+    }
+
+    /*
+     * I added a function getAllWaves which will return the struct array, waves, to us.
+     * This will make it easy to retrieve the waves from our website!
+     */
+    function getAllWaves() public view returns (Wave[] memory) {
+        return waves;
     }
 
     function getTotalWaves() public view returns (uint256) {
+        // Optional: Add this line if you want to see the contract print the value!
+        // We'll also print it over in run.js as well.
         console.log("We have %d total waves!", totalWaves);
         return totalWaves;
     }
--- waveportal-starter-project-main/src/App.js
+++ waveportal-starter-project-main/src/App.js
@@ -1,29 +1,260 @@
-import * as React from "react";
+import React, { useEffect, useState } from "react";
 import { ethers } from "ethers";
-import './App.css';
+import "./App.css";
+import contractAbi from "./utils/WavePortal.json";
 
-export default function App() {
+const CONTRACT_ADDRESS = "0x7bEa7FBB2316F99f9799c792Df5Db800a2b2Df3a";
+const CONTRACT_ABI = contractAbi.abi;
 
-  const wave = () => {
-    
-  }
-  
+const App = () => {
+  const [currentAccount, setCurrentAccount] = useState("");
+  const [totalNumWaves, setTotalNumWaves] = useState(0);
+  const [message, setMessage] = useState("");
+  const [allWaves, setAllWaves] = useState([]);
+  const [isWaving, setIsWaving] = useState(false);
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log("Make sure you have metamask!");
+        return;
+      } else {
+        console.log("We have the ethereum object", ethereum);
+      }
+
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account:", account);
+        setCurrentAccount(account);
+        getAllWaves();
+
+        const wavePortalContract = getContractDetails();
+        let count = await wavePortalContract.getTotalWaves();
+        setTotalNumWaves(count.toNumber());
+      } else {
+        console.log("No authorized account found");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+
+      const wavePortalContract = getContractDetails();
+      let count = await wavePortalContract.getTotalWaves();
+      console.log("COUNT", count.toNumber());
+      setTotalNumWaves(count.toNumber());
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getContractDetails = () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        return new ethers.Contract(CONTRACT_ADDRESS, CONTRACT_ABI, signer);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const wave = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        setIsWaving(true);
+        const wavePortalContract = getContractDetails();
+        let count = await wavePortalContract.getTotalWaves();
+
+        setTotalNumWaves(count.toNumber());
+        console.log("Retrieved total wave count...", count.toNumber());
+
+        const waveTxn = await wavePortalContract.wave(message, {
+          gasLimit: 30000,
+        });
+        console.log("Mining...", waveTxn.hash);
+        setMessage("");
+
+        await waveTxn.wait();
+        console.log("Mined -- ", waveTxn.hash);
+
+        count = await wavePortalContract.getTotalWaves();
+        setTotalNumWaves(count.toNumber());
+
+        console.log("Retrieved total wave count...", count.toNumber());
+        setIsWaving(false);
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const onInputChange = (event) => {
+    const { value } = event.target;
+    setMessage(value);
+  };
+
+  const getAllWaves = async () => {
+    const { ethereum } = window;
+
+    try {
+      if (ethereum) {
+        const wavePortalContract = getContractDetails();
+        const waves = await wavePortalContract.getAllWaves();
+
+        const wavesCleaned = waves.map((wave) => {
+          return {
+            address: wave.waver,
+            timestamp: new Date(wave.timestamp * 1000),
+            message: wave.message,
+          };
+        });
+
+        setAllWaves(wavesCleaned);
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  /**
+   * Listen in for emitter events!
+   */
+  useEffect(() => {
+    checkIfWalletIsConnected();
+    let wavePortalContract;
+
+    const onNewWave = (from, timestamp, message) => {
+      console.log("NewWave", from, timestamp, message);
+      setAllWaves((prevState) => [
+        ...prevState,
+        {
+          address: from,
+          timestamp: new Date(timestamp * 1000),
+          message: message,
+        },
+      ]);
+    };
+
+    if (window.ethereum) {
+      wavePortalContract = getContractDetails();
+      wavePortalContract.on("NewWave", onNewWave);
+    }
+
+    return () => {
+      if (wavePortalContract) {
+        wavePortalContract.off("NewWave", onNewWave);
+      }
+    };
+  }, []);
+
   return (
     <div className="mainContainer">
-
       <div className="dataContainer">
         <div className="header">
-        👋 Hey there!
+          <span>🛩</span> Welcome General!
         </div>
 
         <div className="bio">
-        I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
+          Welcome aboard, captain! In this mission, you'll get a chance to wave
+          at your fellow KoolTravelers. This way, you can all hang and chat and
+          socialize and collaborate
+          <span role="img" aria-label="star">
+            ✨
+          </span>{" "}
+          !
         </div>
+        <div className="bio">So far, we've got</div>
+        <div className="counter">{totalNumWaves}</div>
+        <div className="bio"> wave{totalNumWaves === 1 ? "" : "s"}! </div>
+        <div className="bio">Can we reach 888,888?</div>
 
-        <button className="waveButton" onClick={wave}>
-          Wave at Me
-        </button>
+        {!currentAccount && (
+          <button
+            className="cta-button submit-gif-button"
+            style={{
+              marginTop: "16px",
+              padding: "8px",
+            }}
+            onClick={connectWallet}
+          >
+            Connect Wallet
+          </button>
+        )}
+
+        {currentAccount && (
+          <div className="connected-container">
+            <form
+              onSubmit={(event) => {
+                event.preventDefault();
+                wave();
+              }}
+            >
+              <input
+                type="text"
+                placeholder="Whatchu say?"
+                value={message}
+                onChange={onInputChange}
+              />
+              <button
+                type="submit"
+                className="cta-button submit-gif-button"
+                disabled={isWaving}
+              >
+                {isWaving ? "Sending wave..." : "SEND IT!"}
+              </button>
+            </form>
+          </div>
+        )}
+
+        {allWaves.map((wave, index) => {
+          return (
+            <div
+              key={index}
+              style={{
+                backgroundColor: "OldLace",
+                marginTop: "16px",
+                padding: "8px",
+              }}
+            >
+              <div>Address: {wave.address}</div>
+              <div>Time: {wave.timestamp.toString()}</div>
+              <div>Message: {wave.message}</div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
-}
+};
+
+export default App;
